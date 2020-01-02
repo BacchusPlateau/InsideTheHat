afc7bcca1282ea1344e14d45d357048d5b93e503
Bret Williams <straightener@hotmail.com>
2020-01-01 18:42:35 -0600

    added moving wall

--- InsideTheHat/GameScene.swift
+++ InsideTheHat/GameScene.swift
@@ -13,15 +13,69 @@ class GameScene: SKScene {
     private var rabbit: SKSpriteNode!
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
+    private var wall : SKSpriteNode!
+    private var leftDoor : SKSpriteNode!
+    private var centerDoor : SKSpriteNode!
+    private var rightDoor : SKSpriteNode!
     
     override func didMove(to view: SKView) {
         
         self.initializeMainCharacter()
+        self.initializeWall()
+        self.initializeWallMovement()
+        self.initializeDoors()
         
     }
     
     func distanceBetween(point p1: CGPoint, andPoint p2:CGPoint) -> CGFloat {
         return sqrt(pow((p2.x - p1.x), 2) + pow((p2.y - p1.y), 2))
+    }
+    
+    func initializeDoors() {
+        
+        
+        
+    }
+    
+    func initializeMainCharacter() {
+        
+        rabbit = SKSpriteNode(imageNamed: "rabbit")
+        rabbit.position = CGPoint(x: (view!.bounds.size.width / 2), y: rabbit.size.height)
+        
+        let background = SKSpriteNode(imageNamed: "background")
+        background.anchorPoint = .zero
+        background.zPosition = -1
+        
+        addChild(background)
+        addChild(rabbit)
+        
+    }
+    
+    func initializeWall() {
+        
+        wall = SKSpriteNode(imageNamed: "wall")
+        wall.position = CGPoint(x: (view!.bounds.size.width / 2), y: view!.bounds.size.height + wall.frame.size.height / 2)
+        wall.zPosition = 2
+        addChild(wall)
+        
+    }
+    
+    func initializeWallMovement() {
+        
+        let wallSpeed: CGFloat = 250.0
+        let nextWallPosition = CGPoint(x: wall.position.x, y: -wall.frame.size.height / 2)
+        let duration = self.distanceBetween(point: wall.position, andPoint: nextWallPosition) / wallSpeed
+        let moveWallAction = SKAction.moveTo(y: nextWallPosition.y, duration: Double(duration))
+        let resetPositionAction = SKAction.run {
+            self.wall.position = CGPoint(x: (self.view!.bounds.size.width / 2), y: self.view!.bounds.size.height +
+             self.wall.frame.size.height / 2)
+        }
+            
+        let delayAction = SKAction.wait(forDuration: 2.0)
+        let sequence = SKAction.sequence([moveWallAction, resetPositionAction, delayAction])
+        
+        wall.run(SKAction.repeatForever(sequence))
+        
     }
     
     func moveRabbitToNextLocation(touchLocation: CGPoint) {
@@ -46,20 +100,6 @@ class GameScene: SKScene {
         
     }
     
-    func initializeMainCharacter() {
-        
-        rabbit = SKSpriteNode(imageNamed: "rabbit")
-        rabbit.position = CGPoint(x: (view!.bounds.size.width / 2), y: rabbit.size.height)
-        
-        let background = SKSpriteNode(imageNamed: "background")
-        background.anchorPoint = .zero
-        background.zPosition = -1
-        
-        addChild(background)
-        addChild(rabbit)
-        
-    }
-    
     func touchDown(atPoint pos : CGPoint) {
         
     }
@@ -75,6 +115,11 @@ class GameScene: SKScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         if let touch = touches.first {
+            
+            if rabbit.hasActions() {
+                rabbit.removeAllActions()
+            }
+            
             let location = touch.location(in: self)
             self.moveRabbitToNextLocation(touchLocation: location)
         }
